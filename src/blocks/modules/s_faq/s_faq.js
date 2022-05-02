df573ce995fc99cae870ec7179c799cc48c0212e
Bochkarev Ivan <ivanx86@gmail.com>
2022-05-02 10:13:35 +0600

    Рефакторинг s-faq

--- src/blocks/modules/s_faq/s_faq.js
+++ src/blocks/modules/s_faq/s_faq.js
@@ -1,27 +1,20 @@
 
 window.addEventListener("load", function (event) {
-  const tabs = document.querySelectorAll('.accordion-item__question'),
-    tabContents = document.querySelectorAll('.accordion-item__answer'),
-    accordionBtns = document.querySelectorAll(".accordion-item__answer");
+  const questions = document.querySelectorAll('.accordion-item__question'),
+    answer = document.querySelectorAll('.accordion-item__answer');
 
-  tabs.forEach(tab => {
+  questions.forEach(tab => {
     tab.addEventListener('click', () => {
       const target = document.querySelector('#' + tab.dataset.tabTarget)
 
-      tabContents.forEach(tabContent => {
+      answer.forEach(tabContent => {
         tabContent.classList.remove('is-active')
       })
-      tabs.forEach(tab => {
+      questions.forEach(tab => {
         tab.classList.remove('is-active')
       })
       tab.classList.add('is-active')
       target.classList.add('is-active')
     })
   })
-
-  // tabs.forEach((accordion) => {
-  //   accordion.onclick = function () {
-  //     this.classList.toggle("is-active");
-  //   };
-  // });
 });
