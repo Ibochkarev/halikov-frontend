0d860b29b49df95dbb937c209781fad040c0bb22
Bochkarev Ivan <ivanx86@gmail.com>
2022-05-22 17:16:30 +0600

    Добавлен swiper + fancybox 4

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,7 @@
+import { Fancybox as NativeFancybox } from "@fancyapps/ui/dist/fancybox.esm.js";
+import ru from "@fancyapps/ui/src/Fancybox/l10n/ru/";
+NativeFancybox.defaults.l10n = ru;
+NativeFancybox.defaults.autoFocus = false;
+
 import "./import/modules";
 import "./import/components";
