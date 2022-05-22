517a40b0d3ef43ce1eebdb7223ef916cc3f17749
Bochkarev Ivan <ivanx86@gmail.com>
2022-05-22 17:08:54 +0600

    Добавлен миксин для вывода форм

--- src/blocks/modules/s_contacts/s_contacts.js
+++ src/blocks/modules/s_contacts/s_contacts.js
@@ -1,24 +1,26 @@
-window.onload = function () {
-  let mapTitle = document.createElement('div');
-  const wrapMap = document.querySelector('#wrapMap');
+if (document.querySelector('.s-contacts__map')) {
+  window.onload = function () {
+    let mapTitle = document.createElement('div');
+    const wrapMap = document.querySelector('#wrapMap');
 
-  mapTitle.className = 'mapTitle';
-  mapTitle.textContent = 'Для активации карты нажмите по ней';
+    mapTitle.className = 'mapTitle';
+    mapTitle.textContent = 'Для активации карты нажмите по ней';
 
-  wrapMap.appendChild(mapTitle);
+    wrapMap.appendChild(mapTitle);
 
-  wrapMap.onclick = function () {
-    this.children[0].removeAttribute('style');
-    mapTitle.parentElement.removeChild(mapTitle);
-  }
+    wrapMap.onclick = function () {
+      this.children[0].removeAttribute('style');
+      mapTitle.parentElement.removeChild(mapTitle);
+    }
 
-  wrapMap.onmousemove = function (event) {
-    mapTitle.style.display = 'block';
-    if (event.offsetY > 10) mapTitle.style.top = event.offsetY + 20 + 'px';
-    if (event.offsetX > 10) mapTitle.style.left = event.offsetX + 20 + 'px';
-  }
+    wrapMap.onmousemove = function (event) {
+      mapTitle.style.display = 'block';
+      if (event.offsetY > 10) mapTitle.style.top = event.offsetY + 20 + 'px';
+      if (event.offsetX > 10) mapTitle.style.left = event.offsetX + 20 + 'px';
+    }
 
-  wrapMap.onmouseleave = function () {
-    mapTitle.style.display = 'none';
-  }
-};
+    wrapMap.onmouseleave = function () {
+      mapTitle.style.display = 'none';
+    }
+  };
+}
